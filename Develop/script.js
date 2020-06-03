c6a3cfe4cad04d76fa5ca8eef8656c1de0fb5965
mr-cade <cadelpetersen@gmail.com>
2020-06-03 13:15:15 -0600

    README update, data validation messages

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var retVal = ""
+
 
 function generatePassword() {
+// input prompt for password length
   var pwLength = prompt("input number of characters 8-128")
     if(pwLength < 8 || pwLength > 128) {
       alert("Please select a number of characters between 8-128");
-      prompt("input number of characters 8-128");
+      return "Please try again";
     } 
-  var special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 // if statements to turn on/off character types in variable charset(line 48)
   var lowerCase = confirm("include lowercase letters?");
@@ -15,42 +17,42 @@ function generatePassword() {
       lowerCase = "abcdefghijklmnopqrstuvwxyz"
     } else {
       lowerCase = ''
-    };
+  };
         
-    var upperCase = confirm("include upper case letters?");
-    if (upperCase === true) {
-      upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    } else {
-      upperCase = ''
-    };
+  var upperCase = confirm("include upper case letters?");
+  if (upperCase === true) {
+    upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  } else {
+    upperCase = ''
+  };
 
-    var nums = confirm("include numbers?");
-    if (nums === true) {
-      nums = "0123456789"
-    } else {
-      nums = ''
-    };
+  var nums = confirm("include numbers?");
+  if (nums === true) {
+    nums = "0123456789"
+  } else {
+    nums = ''
+  };
 
-    var special = confirm("include special characters?");
-    if (special === true) {
-      special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
-    } else {
-      special = ''
-    };
+  var special = confirm("include special characters?");
+  if (special === true) {
+    special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  } else {
+    special = ''
+  };
 
-  var retVal = ""
-  
+  // concatenate chosen characters into charset string
+    var charset = lowerCase + upperCase + nums + special 
+    console.log(charset)
+    if (charset === '') {
+      return "Please select at least one type of character."
+    }
 
-// concatenate chosen characters into charset string
-  var charset = lowerCase + upperCase + nums + special 
-  console.log(charset)
-
-// assemble password for given number of characters
-  for(var i = 0; i <= pwLength; i++) {
-    retVal += charset.charAt(Math.floor(Math.random() * charset.length));
+  // assemble password for given number of characters
+    for(var i = 0; i <= pwLength; i++) {
+      retVal += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    return retVal
   }
-  return retVal
-}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
